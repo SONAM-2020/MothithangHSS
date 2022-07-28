960b76e70c037de3ad11cf14c2ecf0ff438b7ea4
Tandin Dorji <12210091.gcit@rub.edu.bt>
2022-07-28 13:39:07 +0600

    sdf

---
+++ application/views/web/pages/contactus.php
@@ -0,0 +1,41 @@
+<div class="img-container">
+        <img src="../image/contactus.png" alt="image" class="image1">
+        <p class="f_contactus">Contact Us</p>
+    </div>
+   
+    
+    <div class="contact">
+        <div class="location">
+            <span class="icon"> <i class="fa-solid fa-location-dot fa-3x"></i> </span>
+            <p class="header">
+                Office Address
+            </p>
+            
+            <p class="content"> 
+                Motithang, Thimphu
+            </p>
+            
+        </div>
+        <div class="call">
+            <span class="icon"> <i class="fa-solid fa-phone fa-3x"></i> </span>
+            <p class="header">
+                Call Us
+            </p>
+
+            <p class="content">
+                Tel:+975-123456/123456
+            </p>
+        </div>
+
+        <div class="message">
+            <span class="icon"> <i class="fa-solid fa-message fa-3x"> </i> </span>
+            <p class="header">
+                Message Us
+            </p>
+            
+            <p class="content">
+                Email: <a href="mailto:motithang@gov.edu.bt">motithang@gov.edu.bt</a> 
+            </p>
+            
+        </div>
+    </div>
