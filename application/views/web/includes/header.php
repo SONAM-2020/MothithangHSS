177372fc84e448f29e6d63f48f5ec1f9820f9258
SONAM DORJI <sdorji815@gmail.com>
2022-08-02 16:08:30 +0600

    front page upodate

--- application/views/web/includes/header.php
+++ application/views/web/includes/header.php
@@ -1,29 +1,3 @@
-<!doctype html>
-<html class="no-js" lang="zxx">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Motithang Higher Secondary School</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url();?>uploads/dcsilogo.png">
-    <link rel="stylesheet" href="<?php echo base_url();?>assest/website/about.css">
-    <link rel="stylesheet" href="<?php echo base_url();?>assest/website/alumni.css">
-    <link rel="stylesheet" href="<?php echo base_url();?>assest/website/announcement.css">
-    <link rel="stylesheet" href="<?php echo base_url();?>assest/website/belowNav.css">
-    <link rel="stylesheet" href="<?php echo base_url();?>assest/website/card.css">
-    <link rel="stylesheet" href="<?php echo base_url();?>assest/website/contact.css">
-    <link rel="stylesheet" href="<?php echo base_url();?>assest/website/countrytopper.css">
-    <link rel="stylesheet" href="<?php echo base_url();?>assest/website/footer.css">
-    <link rel="stylesheet" href="<?php echo base_url();?>assest/website/history.css">
-    <link rel="stylesheet" href="<?php echo base_url();?>assest/website/home.css">
-    <link rel="stylesheet" href="<?php echo base_url();?>assest/website/nav.css">
-    <link rel="stylesheet" href="<?php echo base_url();?>assest/website/news.css">
-    <link rel="stylesheet" href="<?php echo base_url();?>assest/website/newsEvents.css">
-    <link rel="stylesheet" href="<?php echo base_url();?>assest/website/result.css">
-   
-</head>
-
 <!DOCTYPE html>
 <html lang="en">
 
--- application/views/web/includes/nevagation.php
+++ application/views/web/includes/nevagation.php
@@ -63,93 +63,38 @@
                             <a href="index.html"><img src="assets/images/logo.png" alt=""></a>
                         </div>
                         <ul class="nav navbar-nav"> 
-                            <li class="active"><a href="javascript:;">Home <i class="fa fa-chevron-down"></i></a>
+                            <li class="active"><a href="#">Home</a>
+                            </li>
+                            <li><a href="javascript:;">Announcement <i class="fa fa-chevron-down"></i></a>
                                 <ul class="sub-menu">
-                                    <li><a href="index.html">Home 1</a></li>
-                                    <li><a href="index-2.html">Home 2</a></li>
+                                    <li><a href="#" onclick="loadpage('<?php echo base_url();?>index.php?baseController/loadpage/aboutus/')">News & Events</a></li>
+                                    <li><a href="#" onclick="loadpage('<?php echo base_url();?>index.php?baseController/loadpage/aboutus/')">Announcement</a></li>
                                 </ul>
                             </li>
-                            <li><a href="javascript:;">Pages <i class="fa fa-chevron-down"></i></a>
+                            <li><a href="javascript:;">Non-Academic <i class="fa fa-chevron-down"></i></a>
                                 <ul class="sub-menu">
-                                    <li><a href="javascript:;">About<i class="fa fa-angle-right"></i></a>
-                                        <ul class="sub-menu">
-                                            <li><a href="#" onclick="loadpage('<?php echo base_url();?>index.php?baseController/loadpage/aboutus/')">About Us</a></li>
-                                            <li><a href="events-details.html">Events Details</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="javascript:;">Event<i class="fa fa-angle-right"></i></a>
-                                        <ul class="sub-menu">
-                                            <li><a href="event.html">Event</a></li>
-                                            <li><a href="events-details.html">Events Details</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="javascript:;">FAQ's<i class="fa fa-angle-right"></i></a>
-                                        <ul class="sub-menu">
-                                            <li><a href="faq-1.html">FAQ's 1</a></li>
-                                            <li><a href="faq-2.html">FAQ's 2</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="javascript:;">Contact Us<i class="fa fa-angle-right"></i></a>
-                                        <ul class="sub-menu">
-                                            <li><a href="#" onclick="loadpage('<?php echo base_url();?>index.php?baseController/loadpage/contactus/')" >Contact Us 1</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="portfolio.html">Portfolio</a></li>
-                                    <li><a href="profile.html">Profile</a></li>
-                                    <li><a href="membership.html">Membership</a></li>
-                                    <li><a href="error-404.html">404 Page</a></li>
+                                    <li><a href="#" onclick="loadpage('<?php echo base_url();?>index.php?baseController/loadpage/aboutus/')">Rule</a></li>
+                                    <li><a href="#" onclick="loadpage('<?php echo base_url();?>index.php?baseController/loadpage/aboutus/')">Policy</a></li>
+                                    <li><a href="#" onclick="loadpage('<?php echo base_url();?>index.php?baseController/loadpage/aboutus/')">Alumini</a></li>
                                 </ul>
                             </li>
-                            <li class="add-mega-menu"><a href="javascript:;">Our Courses <i class="fa fa-chevron-down"></i></a>
-                                <ul class="sub-menu add-menu">
-                                    <li class="add-menu-left">
-                                        <h5 class="menu-adv-title">Our Courses</h5>
-                                        <ul>
-                                            <li><a href="courses.html">Courses </a></li>
-                                            <li><a href="courses-details.html">Courses Details</a></li>
-                                            <li><a href="profile.html">Instructor Profile</a></li>
-                                            <li><a href="event.html">Upcoming Event</a></li>
-                                            <li><a href="membership.html">Membership</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="add-menu-right">
-                                        <img src="assets/images/adv/adv.jpg" alt=""/>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li><a href="javascript:;">Blog <i class="fa fa-chevron-down"></i></a>
+                            <li><a href="javascript:;">Academic <i class="fa fa-chevron-down"></i></a>
                                 <ul class="sub-menu">
-                                    <li><a href="blog-classic-grid.html">Blog Classic</a></li>
-                                    <li><a href="blog-classic-sidebar.html">Blog Classic Sidebar</a></li>
-                                    <li><a href="blog-list-sidebar.html">Blog List Sidebar</a></li>
-                                    <li><a href="blog-standard-sidebar.html">Blog Standard Sidebar</a></li>
-                                    <li><a href="blog-details.html">Blog Details</a></li>
+                                    <li><a href="#" onclick="loadpage('<?php echo base_url();?>index.php?baseController/loadpage/aboutus/')">Result</a></li>
+                                    <li><a href="#" onclick="loadpage('<?php echo base_url();?>index.php?baseController/loadpage/aboutus/')">Downloads</a></li>
+                                    <li><a href="#" onclick="loadpage('<?php echo base_url();?>index.php?baseController/loadpage/aboutus/')">Academic Topper</a></li>
+                                    <li><a href="#" onclick="loadpage('<?php echo base_url();?>index.php?baseController/loadpage/aboutus/')">Academic Calender</a></li>
                                 </ul>
                             </li>
-                            <li class="nav-dashboard"><a href="javascript:;">Dashboard <i class="fa fa-chevron-down"></i></a>
+                            <li><a href="javascript:;">About Us <i class="fa fa-chevron-down"></i></a>
                                 <ul class="sub-menu">
-                                    <li><a href="admin/index.html">Dashboard</a></li>
-                                    <li><a href="admin/add-listing.html">Add Listing</a></li>
-                                    <li><a href="admin/bookmark.html">Bookmark</a></li>
-                                    <li><a href="admin/courses.html">Courses</a></li>
-                                    <li><a href="admin/review.html">Review</a></li>
-                                    <li><a href="admin/teacher-profile.html">Teacher Profile</a></li>
-                                    <li><a href="admin/user-profile.html">User Profile</a></li>
-                                    <li><a href="javascript:;">Calendar<i class="fa fa-angle-right"></i></a>
-                                        <ul class="sub-menu">
-                                            <li><a href="admin/basic-calendar.html">Basic Calendar</a></li>
-                                            <li><a href="admin/list-view-calendar.html">List View Calendar</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="javascript:;">Mailbox<i class="fa fa-angle-right"></i></a>
-                                        <ul class="sub-menu">
-                                            <li><a href="admin/mailbox.html">Mailbox</a></li>
-                                            <li><a href="admin/mailbox-compose.html">Compose</a></li>
-                                            <li><a href="admin/mailbox-read.html">Mail Read</a></li>
-                                        </ul>
-                                    </li>
+                                    <li><a href="#" onclick="loadpage('<?php echo base_url();?>index.php?baseController/loadpage/aboutus/')">About Mothithang</a></li>
+                                    <li><a href="#" onclick="loadpage('<?php echo base_url();?>index.php?baseController/loadpage/aboutus/')">Staff Profile</a></li>
+                                    <li><a href="#" onclick="loadpage('<?php echo base_url();?>index.php?baseController/loadpage/aboutus/')">History</a></li>
+                                    <li><a href="#" onclick="loadpage('<?php echo base_url();?>index.php?baseController/loadpage/aboutus/')">Mothithang Finist</a></li>
                                 </ul>
                             </li>
+                            <li class="active"><a href="#" onclick="loadpage('<?php echo base_url();?>index.php?baseController/loadpage/aboutus/')">Contact Us</a>
                         </ul>
                         <div class="nav-social-link">
                             <a href="javascript:;"><i class="fa fa-facebook"></i></a>
